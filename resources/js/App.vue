fdfe8964d40e2afbdd5e6f9a2e746bc982daed17
Yu Moua <ucoder80@example.com>
2026-02-02 19:56:56 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>Hello Vue 3 with Laravel!</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiniPOS17</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Hello Laravel</h1>
+    <div id="app-vue"></div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
