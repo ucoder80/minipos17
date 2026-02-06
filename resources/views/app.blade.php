251d03b076e455975b14ca0cd38080fc71850b05
Yu Moua <ucoder80@example.com>
2026-02-06 19:28:36 +0700

    Lesion 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MiniPOS17</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="assets/css/font_lao.css">
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css  -->
+    
+    <link rel="stylesheet" href="assets/vendor/css/core.css">
+    <link rel="stylesheet" href="assets/css/demo.css">
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+    <!-- endbuild -->
+    <link rel="stylesheet" href="assets/vendor/libs/apex-charts/apex-charts.css">
+
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hello Laravel</h1>
     <div id="app-vue"></div>
 
+    <!-- Core JS -->
+    <script src="assets/vendor/libs/jquery/jquery.js"></script>   
+    <script src="assets/vendor/libs/popper/popper.js"></script>
+    <script src="assets/vendor/js/bootstrap.js"></script>
+    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="assets/vendor/js/menu.js"></script>
+    
+    <!-- endbuild -->
     @vite('resources/js/app.js')
 </body>
 </html>
